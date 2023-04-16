378492ffa06be9c0515adae475de9af13fc9933c
Rafi <smurfoxalion@gmail.com>
2023-04-16 21:55:09 +0700

    asd

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../components/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer_up">
+        <div className="footer_content">
+          <h1>
+            What Are You Waiting For? Let's Join The <br />
+            TimTam Community
+          </h1>
+          <p>
+            TimTam hadir dengan konten untuk membantu meningkatkan kontrol diri,
+            mengurangi kecemasan dan memberikan saran praktis untuk mengatasi
+            gejala-gejala kecemasan.
+          </p>
+        </div>
+        <div className="footer_button">
+          <Link to="/about">
+            <button>See More</button>
+          </Link>
+        </div>
+      </div>
+
+      <div className="footer_down">
+        <div className="down_left">
+          <h1>TIMTAM</h1>
+          <p>
+            TimTam hadir dengan konten untuk membantu meningkatkan kontrol diri,
+            mengurangi kecemasan dan memberikan saran praktis untuk mengatasi
+            gejala-gejala kecemasan.
+          </p>
+          <Link to="/about" className="down_btn">
+            <button>Contact Us</button>
+          </Link>
+        </div>
+        <div className="down_right">
+            <div className="ilnks">
+                <h1>TimTam</h1>
+                <p>Tentang Kami</p>
+                <p>Blog</p>
+            </div>
+            <div className="ilnks">
+                <h1>Layanan</h1>
+                <p>Game</p>
+                <p>Jurnal Online</p>
+                <p>Komunitas</p>
+            </div>
+            <div className="ilnks">
+                <h1>Bantuan dan Panduan</h1>
+                <p>Syarat dan Ketentuan</p>
+                <p>Kebijakan Privasi</p>
+                <p>Bantuan</p>
+                <div className="socmed">
+                    <img src="../../assets/instagram.png" alt="" />
+                    <img src="../../assets/linkedin.png" alt="" />
+                    <img src="../../assets/youtube.png" alt="" />
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
